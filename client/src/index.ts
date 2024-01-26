83bcb9738f20e174c5e5f11f0b8a353faadf2d76
Sérgio <sergioseco98@gmail.com>
2024-01-26 18:43:52 +0000

    Get near account working

--- client/src/index.ts
+++ client/src/index.ts
@@ -37,54 +37,66 @@ export async function sendTransaction(action: any) {
 
 export const getNearAccount = async (
     network: string,
-    keys?: KeyPair[],
-): Promise<Account | null> => {
+    keys?: [nearAPI.KeyPair, nearAPI.KeyPair],
+): Promise<nearAPI.Account | null> => {
 
-    let correctKey = null
-    let accountId = null
+    const {keyPair, accountId} = await getCorrectPublicKey(keys);
 
-    for (const _key of keys) {
-        const publicKeyString = _key.getPublicKey()?.toString();
+    if (!keyPair) {
+        console.error('No correct key found for the given account ID.');
+        return null;
+    }
 
-        if (!publicKeyString) {
-            return;
-        }
+    const keyStore = new nearAPI.keyStores.InMemoryKeyStore();
+    await keyStore.setKey(network, accountId, keyPair);
+
+    const networkConfig = {
+        networkId: network,
+        nodeUrl: `https://rpc.${network}.near.org`,
+        walletUrl: `https://wallet.${network}.near.org`,
+        helperUrl: `https://helper.${network}.near.org`,
+    };
+
+    const near = new nearAPI.Near({
+        ...networkConfig,
+        deps: { keyStore },
+    });
+
+    return new nearAPI.Account(near.connection, accountId);
+};
+
+export const getCorrectPublicKey = async (keys: [nearAPI.KeyPair, nearAPI.KeyPair], username?: string): Promise<{keyPair: nearAPI.KeyPair, accountId: string}> => {
+    for (const key of keys) {
+        const publicKeyString = key.getPublicKey()?.toString();
         const { data } = await accountsByPublicKey(publicKeyString);
 
-        if (data.length > 0) {
-            accountId = data?.[0]
-            correctKey = _key;
-            break;
+        const isValidKey = username ? data.some((id) => id === username) : data.length > 0;
+
+        if (isValidKey) {
+            return {
+                keyPair: key,
+                accountId: data?.[0]
+            }
         }
     }
 
-
-if (!correctKey) {
-    console.error('No correct key found for the given account ID.');
-    return null;
-}
-
-const keyStore = new keyStores.InMemoryKeyStore();
-await keyStore.setKey(network, accountId, correctKey);
-
-const networkConfig = {
-    networkId: network,
-    nodeUrl: `https://rpc.${network}.near.org`,
-    walletUrl: `https://wallet.${network}.near.org`,
-    helperUrl: `https://helper.${network}.near.org`,
+    throw new Error("No account found for key");
 };
 
-const near = new Near({
-    ...networkConfig,
-    deps: { keyStore },
-});
+export const getMintAction = (media: string, reference: string, nft_contract_id: string): any => {
+    const obj: FunctionCallAction = {
+        type: "FunctionCall",
+        params: {
+            methodName: "mint",
+            args: {
+                metadata: {media:"dXQsx5qnoRxnAeNmZA3T3N-Q2WmbVzBwUooclwUKlgw"},
+                nft_contract_id: "drop.mintbase1.near"
+            },
+            gas: "200000000000000",
+            deposit: "10000000000000000000000"
+        }
+    };
+    return obj;
+}
 
-return new Account(near.connection, accountId);
-  };
 
-//getKey
-//create action
-//call geNearAccount
-//create delegate
-//sign delegate
-//send to relayer
