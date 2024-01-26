eefaf2f3c396b1d6e6bfc6762b410e19c788a433
Sérgio <sergioseco98@gmail.com>
2024-01-26 18:44:22 +0000

    Started relayTransaction method

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,12 +1,9 @@
-//biometric createKey
-//send key to server
-//add create account route to server
-
 import { createKey, getKeys } from "@near-js/biometric-ed25519";
-import { Action } from "@near-wallet-selector/core";
-import { Account, KeyPair, Near, keyStores } from "near-api-js";
+import * as nearAPI from "near-api-js";
 import { accountsByPublicKey } from '@mintbase-js/data';
-
+import { encodeSignedDelegate } from "@near-js/transactions";
+import { Action, FunctionCallAction } from "@near-wallet-selector/core";
+import { createAction } from "@near-wallet-selector/wallet-utils";
 
 /**
  * Generates a new keypair locally and stores it in passkey and then sends the
@@ -36,12 +33,50 @@ export async function createAccount(relayerUrl: string, accountId: string): Prom
     return await result.json();
 }
 
-export async function sendTransaction(action: any) {
-    const keys = await getKeys('surge.near');
-    console.log("HERE")
-    getNearAccount('testnet', keys)
+/**
+ * Signs a transaction and sends it to a relayer to get dispatched 
+ * 
+ * @param {Action[]} action - The list of actions to include in the transaction.
+ * @param {string} receiverId - The ID of the receiver.
+ * @param {string} network - tesnet | mainnet.
+ * @returns {Promise<any>} - Most likely a receipt (depends on format being returned by relayer).
+ * @throws {Error} - If there is an error relaying the transaction.
+ */
+export async function relayTransaction(action: any) {
+    const keys = await getKeys('this-shouldnt-be-required');
+    const account = await getNearAccount('mainnet', keys);
 
+    //test
+    console.log(account)
+    console.log(action)
+    
+    const x = await account.addKey("fhjdskjfhsdfjsdpfjdlçsçlsdf")
+
+    // const signedDelegate = await account.signedDelegate({
+    //     actions: [createAction(action)],
+    //     blockHeightTtl: 60,
+    //     receiverId: '1.minsta.mintbus.near',
+    // })
+
+    // console.log(signedDelegate)
+
+    // try {
+    //     const res = await fetch("/api/relay", {
+    //         method: "POST",
+    //         mode: "cors",
+    //         body: JSON.stringify(Array.from(encodeSignedDelegate(signedDelegate))),
+    //         headers: new Headers({ "Content-Type": "application/json" }),
+    //     });
+
+    //     if (res.ok) {
+    //         const result = await res.json();
+    //         return result;
+    //     }
+    // } catch (e) {
+    //     throw new Error('Error relaying transaction');
+    // }
 }
+
 
 export const getNearAccount = async (
     network: string,
